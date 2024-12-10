94b4a2f011517cf61fa9f0da7c975e2f3e8b44f5
alonso170000 <alonsocouoh56@gmail.com>
2024-12-10 11:32:33 -0500

    Update conexion.php

--- mascotas/conexion.php
+++ mascotas/conexion.php
@@ -3,9 +3,10 @@ $servername = getenv("MYSQLHOST");
 $username = getenv("MYSQLUSER");
 $password = getenv("MYSQLPASSWORD");
 $dbname = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT") ?: 3306;
 
 // Crear la conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Verificar la conexión
 if ($conn->connect_error) {
